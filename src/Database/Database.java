fe14ad95a2e09be5bfaa1efcc88ee1d238ea4509
Måns <Mns@Mns-Notebook>
2016-02-20 22:24:28 +0100

    Added Database packet with first version of Database and Record classes.

---
+++ src/Database/Database.java
@@ -0,0 +1,78 @@
+package Database;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * A serializable database containing a list of medical records.
+ * @author Måns
+ *
+ */
+public class Database implements Serializable {
+
+	private ArrayList<Record> recordList;
+	private int idCounter;
+	
+	/**
+	 * Creates a new empty database consisting of an arraylist of Record objects.
+	 */
+	public Database() {
+		recordList = new ArrayList<Record>();
+		idCounter = 0;
+	}
+	
+	/**
+	 * Craetes a 
+	 * @param patientName Name of the patient the record belong to.
+	 * @param doctor Name of the associated doctor.
+	 * @param nurse Name of the associated nurse.
+	 * @param data Medical data string.
+	 */
+	public void createRecord(String patientName, String doctor, String nurse, String data) {
+		recordList.add(new Record(patientName, doctor, nurse, data, idCounter));
+		idCounter++;
+	}
+	
+	/**
+	 * Retrieves the record with the specified id.
+	 * @param id
+	 * @return Returns the record object if found within the databse. If not found null is returned.
+	 */
+	public Record getRecordById(int id) {
+		for(Record r: recordList) {
+			if(r.getId() == id) {
+				return r;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Tries to find the record with the specified record id. If found the record will be deleted from the database.
+	 * @param id Record id of the record to be deleted.
+	 * @return Returns true if specified record is deleted. If not deleted false is returned.
+	 */
+	public boolean deleteRecord(int id) {
+		Record tempRecord = getRecordById(id);
+		if(tempRecord != null) {
+			recordList.remove(tempRecord);
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Retrieves all records belonging to a specified patient in an arraylist.
+	 * @param patientName
+	 * @return Arraylist of all records belonging to patientName.
+	 */
+	public ArrayList<Record> getRecords(String patientName) {
+		ArrayList<Record> tempList = new ArrayList<Record>(); //Create empty record list.
+		
+		for(Record r: recordList) { //Iterate through all records in database.
+			if(r.getPatient() == patientName) //If record belong to specified patient:
+				tempList.add(r); //Add record to list.
+		}
+		return tempList; //Return list of all record belonging to specified patient.
+	}
+}
---
+++ src/Database/Record.java
@@ -0,0 +1,44 @@
+package Database;
+
+/**
+ * A medical record for a specific patient. A nurse and a doctor is associated with the record.
+ * The record contains medical data which can be appended(?) by the associated nurse and doctor.
+ * Simple data structure. Contains get functions to retrieve data and a function to append(?) data.
+ * Each record has an unique record id.
+ * @author Måns
+ *
+ */
+public class Record {
+	
+	int id;
+	private String patient, doctor, nurse, data;
+	
+	public String getPatient() { return patient; }
+	public String getDoctor() { return doctor; }
+	public String getNurse() { return nurse; }
+	public String getData() { return data; }
+	public int getId() { return id };
+	
+	/**
+	 * Appends a data string to the medical data string.
+	 * @param data
+	 */
+	public void writeData(String data) {
+		this.data += data; //Hur skall detta implementeras?
+	}
+	
+	/**
+	 * Creates a new Record file with the specified parameters.
+	 * @param patient
+	 * @param doctor
+	 * @param nurse
+	 * @param data
+	 */
+	public Record(String patient, String doctor, String nurse, String data, int id) {
+		this.patient = patient;
+		this.doctor = doctor;
+		this.nurse = nurse;
+		this.data = data;
+		this.id = id;
+	}
+}
